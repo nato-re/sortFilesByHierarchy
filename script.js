1c3a0de96241f566e0432cd4511dd18a4323b5c0
natox <alvares.renato@hotmail.com>
2020-04-06 20:20:49 -0300

    fileList Variable on JS, still not refined

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ submitButton.addEventListener('click', () => {
 document.getElementById("filepicker").addEventListener("change", function(event) {
   let output = document.getElementById("listing");
   let files = event.target.files;
+  let fileList = Object.values(files);
+  console.log(Object.values(files));
 
   for (let i=0; i<files.length; i++) {
     let item = document.createElement("li");
     item.innerHTML = files[i].webkitRelativePath;
     output.appendChild(item);
   };
+  return fileList
 }, false);
+console.log(fileList) // AQUIAQUI AQUI. ESSA VARIÁVEL
 function myFunction() 
     {
     for (let z=0; z<listing.length; z++)
