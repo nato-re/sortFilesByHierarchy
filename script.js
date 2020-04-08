67e926cd48a741b9a5e17671c6211975d000efe4
Dainnfromlunia <63201971+Dainnfromlunia@users.noreply.github.com>
2020-04-07 23:45:49 -0300

    Se liga no comentario

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submitButton = document.getElementById('submit')
 let verifyInput;
-var filelist;
+var fileList;
 submitButton.addEventListener('click', () => {
   //verifyInput()
   //listFilesInFolder
@@ -18,9 +18,17 @@ document.getElementById("filepicker").addEventListener("change", function(event)
     output.appendChild(item);
   };
   console.log(fileList.map(e => e.name)) // AQUIAQUI AQUI. ESSA VARIÁVEL
-  return filelist
+  return fileList
+  
 }, false);
-function myFunction() 
+function myFunction() // olha essa função 
   {
-    document.getElementById("artigos").innerHTML = filelist.length;
+    if (!fileList) // caso seja 0 ele volta e não da erro, remove e vê o q rola.
+    {      
+   // Return from the function.
+   // Or handle it in a wiser manner than me.
+   return;
+    }
+    var xoxo = fileList.length;
+    document.getElementById("artigos").innerHTML = xoxo;
   }
