d75ad6d7675bf67e9104a5b9c1029b892a63623c
Dainnfromlunia <63201971+Dainnfromlunia@users.noreply.github.com>
2020-04-07 23:57:39 -0300

    Concertado

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ submitButton.addEventListener('click', () => {
   //listFilesInFolder
 })
 
+
 document.getElementById("filepicker").addEventListener("change", function(event) {
   let output = document.getElementById("listing");
   let files = event.target.files;
-  let fileList = Object.values(files);
+  fileList = Object.values(files);
   console.log(Object.values(files));
 
   for (let i=0; i<files.length; i++) {
@@ -21,14 +22,15 @@ document.getElementById("filepicker").addEventListener("change", function(event)
   return fileList
   
 }, false);
+
 function myFunction() // olha essa função 
-  {
-    if (!fileList) // caso seja 0 ele volta e não da erro, remove e vê o q rola.
-    {      
-   // Return from the function.
-   // Or handle it in a wiser manner than me.
-   return;
-    }
-    var xoxo = fileList.length;
-    document.getElementById("artigos").innerHTML = xoxo;
+{
+  if (!fileList) // caso seja 0 ele volta e não da erro, remove e vê o q rola.
+  {      
+ // Return from the function.
+ // Or handle it in a wiser manner than me.
+ return;
   }
+  var xoxo = fileList.length;
+  document.getElementById("artigos").innerHTML = xoxo;
+}
